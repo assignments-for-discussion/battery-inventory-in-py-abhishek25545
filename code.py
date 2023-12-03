1ffac1bdfcad1fb32eb625ad09d5c5cd375f5b45
abhishek a <113160661+abhishek25545@users.noreply.github.com>
2023-12-03 17:18:30 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,30 @@
+def count_batteries_by_health(present_capacities):
+    counts = {
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
+    # function to the code 
+    for present_capacity in present_capacities:
+        soh =present_capacity / 120 * 100
+        if soh > 80:
+            counts["healthy"] += 1
+        elif soh > 62:
+            counts["exchange"] += 1
+        else:
+            counts["failed"] += 1
+    return counts
+
+
+# main function 
+def test_bucketing_by_health():
+    print("Counting batteries by SoH...\n")
+    present_capacities = [113, 116, 80, 95, 92, 70]
+    counts = count_batteries_by_health(present_capacities)
+    assert(counts["healthy"] == 2)
+    assert(counts["exchange"] == 3)
+    assert(counts["failed"] == 1)
+    print("Done counting :)")
+
+if __name__ == '__main__':
+    test_bucketing_by_health()
